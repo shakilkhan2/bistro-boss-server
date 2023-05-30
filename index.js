41b225e8ae6868b79243fd6469d3e429698a9974
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-05-31 00:12:13 +0600

    deleted

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rawumbi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,21 +40,27 @@ async function run() {
     });
 
     // cart collections apis:
-    app.get("/carts", async(req, res) => {
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      if(!email){
+      if (!email) {
         res.send([]);
       }
-      const query = { email:email };
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-    })
+    });
     app.post("/carts", async (req, res) => {
       const item = req.body;
       // console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -76,9 +82,6 @@ app.listen(port, () => {
   console.log(`Bistro Boss is sleeping on port: ${port}`);
 });
 
-
-
-
 /**
  * ------------------------------
  *       Naming Convention
@@ -90,4 +93,4 @@ app.listen(port, () => {
  * app.patch('/users/:id')
  * app.put('/users/:id')
  * app.delete('/users/:id')
- * */ 
+ * */
